d07228487bc07cc52d32642638ba97b0b7edf15d
MMSG97 <maria.m.mga88@gmail.com>
2024-11-04 21:40:05 +0100

    correction de la variable du userSlice importé dans store.js L.8

--- frontend/src/reduxStore/store.js
+++ frontend/src/reduxStore/store.js
@@ -5,7 +5,7 @@ import userReducer from './userSlice';
 // Combine reducers
 const rootReducer = combineReducers({
     //pp : reducer(avec fonction créée au préalable dans un autre fichier)
-    user: userSlice
+    user: userReducer
     // data: asynchronousReducer
 });
 
